f3b9c60c0834e9d765aed60cde68c95b7434d7f2
satyamkr203 <146471886+satyamkr203@users.noreply.github.com>
2025-12-14 20:20:11 +0530

    Adding features snapshot

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,44 +1,39 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
-import cors from 'cors';
+
+import express from "express";
+import cors from "cors";
+
 import sweetsRoutes from "./routes/sweet.js";
-import authRoutes from './routes/auth.js';
+import authRoutes from "./routes/auth.js";
 import inventoryRoutes from "./routes/inventory.js";
-import { errorHandler } from './middleware/errorMiddleware.js';
+import { errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
-// configuration for production
 const corsOptions = {
-  origin: function (origin, callback) {
-    const allowedOrigins = [
-      "http://localhost:5173",
-      "https://sweet-shop-seven-blush.vercel.app",
-    ];
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  origin: [
+    "http://localhost:5173",
+    "https://sweet-shop-seven-blush.vercel.app",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-  optionsSuccessStatus: 204,
 };
 
 
-app.options('*', cors(corsOptions));
-
 app.use(cors(corsOptions));
+
+
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/sweets', sweetsRoutes);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/sweets", sweetsRoutes);
 app.use("/api/sweets", inventoryRoutes);
-// check 
-app.get('/check', (req, res) => res.json({ ok: true }));
+
+
+app.get("/check", (req, res) => res.json({ ok: true }));
+
 
 app.use(errorHandler);
 
