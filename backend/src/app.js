0c54d5cf913d2c191cf0cf9b66154ee4e8e6004c
satyamkr203 <146471886+satyamkr203@users.noreply.github.com>
2025-12-14 15:52:30 +0530

    Resolve the issue preflight

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,9 +23,13 @@ const corsOptions = {
     }
   },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
   optionsSuccessStatus: 204,
 };
+
+
+app.options('*', cors(corsOptions));
 
 app.use(cors(corsOptions));
 app.use(express.json());
